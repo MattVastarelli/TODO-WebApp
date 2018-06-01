b000e03d891ca3f2d5a21f3996d7e9309e287303
Matthew Vastarelli <mvast1@unh.newhaven.edu>
2018-06-01 17:36:11 -0500

    color change in list
    and fade out and remove lis

--- assets/js/TODOs.js
+++ assets/js/TODOs.js
@@ -3,16 +3,12 @@ function colorlis () {
 
 	for(let i = 0, length1 = lis.length; i < length1; i++){
 		if (i % 2 == 0) {
-			lis[i].style.background = "#eceff1";
+			lis[i].style.background = "#eeeeee";
 		}
 		else {
-			lis[i].style.background = "#cfd8dc";
+			lis[i].style.background = "#e0e0e0";
 		}
 	}
-}
-function delAndColor() {
-	$(this).parent().remove();
-	colorlis();
 }
 //--------------------------------------------------------------
 //this will color all the lis in alternating colors
@@ -20,12 +16,16 @@ colorlis();
 
 //event listener for trash
 $("span").on('click', function(event) {
-	delAndColor();
+	$(this).parent().fadeOut('500', function() {
+		$(this).remove();
+		colorlis();
+	});
 });
 
 //event listener for plus
 $("h1 i").on('click', function(event) {
-
+	$("input").fadeToggle('500', function() {
+	});
 });
 
 //event listener for li
@@ -45,7 +45,10 @@ $("input").keypress(function(event) {
 		colorlis();
 		//add new listener for trash
 		$("span").last().on('click', function(event) {
-			delAndColor();
+			$(this).parent().fadeOut('500', function() {
+				$(this).remove();
+				colorlis();
+			});
 		});
 	}
 });
