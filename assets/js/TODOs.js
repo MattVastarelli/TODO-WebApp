e3b23fa1a3c38cd711265714359b9df1d62f910c
Matthew Vastarelli <mvast1@unh.newhaven.edu>
2018-06-01 12:46:13 -0500

    css
    add the spefic color for the colorlis funtion + background and gerneral color scheme.

--- assets/js/TODOs.js
+++ assets/js/TODOs.js
@@ -3,10 +3,10 @@ function colorlis () {
 
 	for(let i = 0, length1 = lis.length; i < length1; i++){
 		if (i % 2 == 0) {
-			lis[i].style.background = "gray";
+			lis[i].style.background = "#eceff1";
 		}
 		else {
-			
+			lis[i].style.background = "#cfd8dc";
 		}
 	}
 }
